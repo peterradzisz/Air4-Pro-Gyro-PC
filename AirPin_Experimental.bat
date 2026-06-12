ff905979b08571920873e51a8bfc69b91d37bd48
Peter Radziszewski <29036440+peterradzisz@users.noreply.github.com>
2026-06-12 19:32:27 +0200

    Replace branch-switching launchers with AirPin_Lab.bat

--- AirPin_Experimental.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d "%~dp0"
-echo Switching to experimental branch...
-git checkout experimental >nul 2>&1
-python main.py %*
--- AirPin_Stable.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d "%~dp0"
-echo Switching to stable branch...
-git checkout stable >nul 2>&1
-python main.py %*
